f12951ad6e8dbb6d8e3916e51d7e9c34bead624d
Sohaib Touseef <muhammadsohaib2233344@gmail.com>
2024-11-22 18:31:09 +0500

    notfound

--- gsitclone/governor-web-clone/src/app/page.tsx
+++ gsitclone/governor-web-clone/src/app/page.tsx
@@ -1,6 +1,4 @@
 import Courese from "@/components/Courese";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
 import ImageSider from "@/components/ImageSider";
 
@@ -8,11 +6,11 @@ import ImageSider from "@/components/ImageSider";
 export default function Home() {
   return (
    <div>
-    <Header/>
+   
     <HeroSection/>
     <ImageSider/>
     <Courese/>
-    <Footer/>
+   
    </div>
   );
 }
